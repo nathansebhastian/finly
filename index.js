3d8b47d1843b91ba61593300615921689c57e49d
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-04-05 20:34:40 +0700

    Chapter 4: Using EJS Templating Engine for Views

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ const morgan = require('morgan');
 
 const app = express();
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.send('Hello From Node.js');
+  res.render('index', { message: 'Hello From Node.js' });
 });
 
 app.get('/contact', (req, res) => {
-  res.send('The Contact Page');
+  res.render('index', { message: 'The Contact Page' });
 });
 
 app.get('/about', (req, res) => {
-  res.send('The About Page');
+  res.render('index', { message: 'The About Page' });
 });
 
 app.get('*', (req, res) => {
-  res.status(404).send('Not Found');
+  res.status(404).render('index', { message: 'Not Found' });
 });
 
 const PORT = 3000;
